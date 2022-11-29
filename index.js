e950f0208e12b297474276cdd2be0b2c7105005b
connor <connor.botterill@yahoo.co.uk>
2022-11-29 20:36:44 +0000

    feat: implement main index

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+import figlet from "figlet";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import inquirerQuestions from "./utils/inquirerQuestions.js";
+
+console.log(chalk.blue(figlet.textSync("Welcome to PyramidAIO")));
+
+inquirer.prompt(inquirerQuestions).then((answers) => {
+  const { site } = answers;
+
+  if (site === "end") {
+    //run end runner
+  } else if (site === "nakedcph") {
+    //run nakedcph runner
+  } else if (site === "dsml") {
+    //run dsm london runner
+  } else if (site === "kith") {
+    // run kith runner
+  }
+});
