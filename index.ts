1f45604fefad583560b3f8608ae93a26a676b208
connor <connor.botterill@yahoo.co.uk>
2022-12-30 11:18:08 +0000

    refactor: update imports

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 // import figlet from "figlet";
 import chalk from "chalk";
 import inquirer from "inquirer";
-import inquirerQuestions from "./src/utils/inquirerQuestions.js";
-import nakedCphRaffleRunner from "./src/modules/sites/nakedcph/nakedCphRaffleRunner.js";
-import endRaffleRunner from "./src/modules/sites/end/endRaffleRunner.js";
-import dsmRaffleRunner from "./src/modules/sites/DSML/dsmRaffleRunner.js";
+import inquirerQuestions from "./src/utils/inquirerQuestions";
+import nakedCphRaffleRunner from "./src/modules/sites/nakedcph/nakedCphRaffleRunner";
+import endRaffleRunner from "./src/modules/sites/end/endRaffleRunner";
+import dsmRaffleRunner from "./src/modules/sites/DSML/dsmRaffleRunner";
 
 // console.log(chalk.blue(figlet.textSync("Welcome to PyramidAIO")));
 console.log(chalk.blue("Welcome to PyramidAIO"));
