1527a0976a9ea0d67bcc08474a1f8cfa95ba8919
connor <connor.botterill@yahoo.co.uk>
2023-01-30 08:40:36 +0000

    chore: add better logging

---
+++ localIndex.ts
@@ -0,0 +1,22 @@
+import figlet from "figlet";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import inquirerQuestions from "./src/utils/inquirerQuestions";
+import { end, dsm, nakedCph } from "./src/modules/sites";
+
+console.log(chalk.green("Checking license key..."));
+
+console.log(chalk.blue(figlet.textSync("Welcome to Pyramid Raffles")));
+console.log(chalk.green.bold("ACCESS GRANTED."));
+inquirer.prompt(inquirerQuestions).then((answers) => {
+  const { site } = answers;
+
+  if (site === "end") {
+    end();
+  } else if (site === "nakedcph") {
+    nakedCph();
+  } else if (site === "dsml") {
+    dsm();
+    `a`;
+  }
+});
