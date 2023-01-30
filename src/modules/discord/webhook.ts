0cdf3fca09228b5155e40124ab335ac7ba9bdc28
connor <connor.botterill@yahoo.co.uk>
2023-01-30 20:08:30 +0000

    feat: update webhook with dynamic data

--- src/modules/discord/webhook.ts
+++ src/modules/discord/webhook.ts
@@ -1,33 +1,37 @@
-const { EmbedBuilder, WebhookClient } = require("discord.js");
-const dotenv = require("dotenv");
+import { EmbedBuilder, WebhookClient } from "discord.js";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const webhookClient = new WebhookClient({
-  url: process.env.WEBHOOK_URL,
-});
-
-const embed = new EmbedBuilder()
-  .setColor(0x0099ff)
-  .setTitle("Naked Successful Checkout")
-  .setThumbnail("https://i.imgur.com/9ZybmnL.png")
-  .addFields({
-    name: "Raffle Link",
-    value: `test`,
-    inline: true,
-  })
-  .addFields({ name: "Email", value: `test@email.com` })
-  .addFields({ name: "Size", value: "N/A" })
-  .addFields({ name: "Extra Info", value: "Remember to check your emails" })
-  .setTimestamp()
-  .setFooter({
-    text: "PyramidAIO",
-    iconURL: "https://i.imgur.com/cE2Ms9p.jpg",
+const webhook = (site: string, thumbnail: string, email: string) => {
+  const webhookClient = new WebhookClient({
+    url: String(process.env.WEBHOOK_URL),
   });
 
-webhookClient.send({
-  content: "Successful entry",
-  username: "PyramidRaffles",
-  avatarURL: "https://i.imgur.com/cE2Ms9p.jpg",
-  embeds: [embed],
-});
+  const embed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle(`${site} Successful Checkout`)
+    .setThumbnail(thumbnail)
+    .addFields({
+      name: "Raffle Link",
+      value: `test`,
+      inline: true,
+    })
+    .addFields({ name: "Email", value: email })
+    .addFields({ name: "Size", value: "N/A" })
+    .addFields({ name: "Extra Info", value: "Remember to check your emails" })
+    .setTimestamp()
+    .setFooter({
+      text: "PyramidAIO",
+      iconURL: "https://i.imgur.com/cE2Ms9p.jpg",
+    });
+
+  webhookClient.send({
+    content: "Successful entry",
+    username: "Pyramid Raffles",
+    avatarURL: "https://i.imgur.com/cE2Ms9p.jpg",
+    embeds: [embed],
+  });
+};
+
+export default webhook;
