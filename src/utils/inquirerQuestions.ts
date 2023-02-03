786c21f57ba751e8d47d51c0c8d09f3bd75ce459
connor <connor.botterill@yahoo.co.uk>
2023-02-03 08:47:24 +0000

    feat: add footpatrol to inquirier

--- src/utils/inquirerQuestions.ts
+++ src/utils/inquirerQuestions.ts
@@ -3,7 +3,7 @@ const inquirerQuestions = [
     type: "list",
     name: "site",
     message: "Select a module to use",
-    choices: ["End", "NakedCPH", "DSML"],
+    choices: ["End", "NakedCPH", "DSML", "Footpatrol"],
     filter(val: string) {
       return val.toLowerCase();
     },
